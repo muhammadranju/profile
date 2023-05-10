820e5d2cb3cb83a55921a7ddbe31865b2c0f9737
Muhammad Ranju <mdranju23@gmail.com>
2023-05-11 01:30:33 +0600

    prot change

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app/app");
 const server = http.createServer(app);
-const port = 5050;
+
+const port = process.env.post || 5050;
 server.listen(port, () => {
     console.log("App is running on http://localhost:" + port);
 });
